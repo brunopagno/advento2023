04d5e9b2d380507218d7f54deb2026446c4786af
Bruno Pagno <brunopagno@gmail.com>
2023-12-08 19:22:41 +0100

    day 4 part 2

--- src/day4.js
+++ src/day4.js
@@ -15,7 +15,25 @@ function part1(input) {
     .reduce((a, b) => a + b, 0);
 }
 
-function part2(input) {}
+function part2(input) {
+  const lines = input.split("\n");
+
+  const result = Array(lines.length).fill(1);
+
+  lines.forEach((line) => {
+    const [card, numbers] = line.split(":");
+    const cardId = parseInt(card.match(/\d+/g)[0], 10);
+    const [winning, mine] = numbers.split("|").map((n) => n.match(/\d+/g));
+
+    const found = winning.filter((n) => mine.includes(n)).length;
+
+    for (let i = 0; i < found; i++) {
+      result[cardId + i] += result[cardId - 1];
+    }
+  });
+
+  return result.reduce((a, b) => a + b, 0);
+}
 
 module.exports = {
   part1,
