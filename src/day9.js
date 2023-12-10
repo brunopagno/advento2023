6aeaf5aa89ae25d5f41e2b3c8e5bebaa259dfe96
Bruno Pagno <brunopagno@gmail.com>
2023-12-10 11:04:53 +0100

    who would guess that it was the same thing but in reverse?

--- src/day9.js
+++ src/day9.js
@@ -16,6 +16,22 @@ function part1(input) {
     .reduce((a, b) => a + b, 0);
 }
 
-function part2(input) {}
+function part2(input) {
+  return input
+    .split("\n")
+    .map((line) => {
+      const sequence = line.split(" ").map(Number).reverse();
+
+      let next = sequence;
+      let values = [];
+      while (!next.every((v) => v === 0)) {
+        values.push(next[next.length - 1]);
+        next = next.map((v, i) => v - next[i - 1]).slice(1);
+      }
+
+      return values.reduce((a, b) => a + b, 0);
+    })
+    .reduce((a, b) => a + b, 0);
+}
 
 module.exports = { part1, part2 };
--- src/day9.spec.js
+++ src/day9.spec.js
@@ -1,21 +1,19 @@
 const { part1, part2 } = require("./day9");
 
 describe("Day 9", () => {
-  describe("part 1", () => {
-    const testInput = `0 3 6 9 12 15
+  const testInput = `0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45`;
 
+  describe("part 1", () => {
     it("should validate test input", () => {
       expect(part1(testInput)).toEqual(114);
     });
   });
 
-  // describe("part 2", () => {
-  //   const testInput = ``;
-
-  //   it("should validate test input", () => {
-  //     expect(part2(testInput)).toEqual(0);
-  //   });
-  // });
+  describe("part 2", () => {
+    it("should validate test input", () => {
+      expect(part2(testInput)).toEqual(2);
+    });
+  });
 });
