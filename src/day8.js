cbcb2a1c130dcf09b8c1e88590907f0f1d042742
Bruno Pagno <brunopagno@gmail.com>
2023-12-10 10:28:20 +0100

    I dont like math because I am not good at math

--- src/day8.js
+++ src/day8.js
@@ -1,7 +1,5 @@
-function part1(input) {
-  const lines = input.split("\n");
-  const commands = [...lines[0]];
-  const nodes = lines.slice(2).map((line) => {
+function setupNodes(nodeLines) {
+  const nodes = nodeLines.map((line) => {
     const [name, left, right] = line.match(/\w\w\w/g);
     return {
       name,
@@ -14,6 +12,14 @@ function part1(input) {
     node.left = nodes.find((n) => n.name === node.left);
     node.right = nodes.find((n) => n.name === node.right);
   });
+
+  return nodes;
+}
+
+function part1(input) {
+  const lines = input.split("\n");
+  const commands = [...lines[0]];
+  const nodes = setupNodes(lines.slice(2));
 
   let steps = 0;
   let currentNode = nodes.find((n) => n.name === "AAA");
@@ -35,6 +41,51 @@ function part1(input) {
   return steps;
 }
 
-function part2(input) {}
+function part2(input) {
+  const lines = input.split("\n");
+  const commands = [...lines[0]];
+  const nodes = setupNodes(lines.slice(2));
+
+  let currentNodes = nodes.filter((n) => n.name[2] === "A");
+
+  const stepsForThemAll = currentNodes.map((currentNode) => {
+    let steps = 0;
+
+    while (currentNode.name[2] != "Z") {
+      const command = commands[steps % commands.length];
+
+      switch (command) {
+        case "L":
+          currentNode = currentNode.left;
+          break;
+        case "R":
+          currentNode = currentNode.right;
+          break;
+      }
+
+      steps += 1;
+    }
+
+    return steps;
+  });
+
+  function gcd(a, b) {
+    if (b === 0) {
+      return a;
+    } else {
+      return gcd(b, a % b);
+    }
+  }
+
+  function lcm(a, b) {
+    return Math.abs(a * b) / gcd(a, b);
+  }
+
+  function lcmOfList(numbers) {
+    return numbers.reduce(lcm);
+  }
+
+  return lcmOfList(stepsForThemAll);
+}
 
 module.exports = { part1, part2 };
--- src/day8.spec.js
+++ src/day8.spec.js
@@ -12,6 +12,7 @@ DDD = (DDD, DDD)
 EEE = (EEE, EEE)
 GGG = (GGG, GGG)
 ZZZ = (ZZZ, ZZZ)`;
+
       expect(part1(testInput)).toEqual(2);
     });
 
@@ -21,13 +22,25 @@ ZZZ = (ZZZ, ZZZ)`;
 AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)`;
+
       expect(part1(testInput)).toEqual(6);
     });
   });
 
-  // describe("part 2", () => {
-  //   it("should validate test input", () => {
-  //     expect(part2(testInput)).toEqual(0);
-  //   });
-  // });
+  describe("part 2", () => {
+    it("should validate test input", () => {
+      const testInput = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`;
+
+      expect(part2(testInput)).toEqual(6);
+    });
+  });
 });
